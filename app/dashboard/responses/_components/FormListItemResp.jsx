3f593cca9765edd5b8d91b4c029581157f43b025
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-07-18 18:21:45 +0530

    Response dashboard is completed

--- app/dashboard/responses/_components/FormListItemResp.jsx
+++ app/dashboard/responses/_components/FormListItemResp.jsx
@@ -4,7 +4,9 @@ import { Button } from '@/components/ui/button'
 import { db } from '@/configs'
 import { userResponses } from '@/configs/schema'
 import { eq } from 'drizzle-orm'
+import { LoaderPinwheel } from 'lucide-react'
 import React, { useState } from 'react'
+import * as XLSX from 'xlsx';
 
 function FormListItemResp({ jsonForm, formRecord }) {
 
@@ -12,15 +14,30 @@ function FormListItemResp({ jsonForm, formRecord }) {
 
 
     const ExportData = async () => {
+        let jsonData=[]
         setLoading(true)
         const result = await db.select().from(userResponses).where(eq(userResponses.formRef, formRecord.id))
 
-        console.log(result)
+        
         if(result){
+            result.forEach((item)=>{
+                const jsonItem=JSON.parse(item.jsonResponse);
+                jsonData.push(jsonItem)
+            })
             setLoading(false);
         }
+        console.log(jsonData)
+        exportToExcel(jsonData)
     }
 
+
+    const exportToExcel=(jsonData)=>{
+        const worksheet = XLSX.utils.json_to_sheet(jsonData)
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
+
+        XLSX.writeFile(workbook, jsonForm?.formTitle+".xlsx")
+    }
 
     return (
         <div className='border shadow-sm rounded-lg p-4 my-5'>
@@ -30,12 +47,11 @@ function FormListItemResp({ jsonForm, formRecord }) {
             <h2 className='text-sm text-gray-500'>{jsonForm?.formHeading}</h2>
             <hr className='my-4'></hr>
             <div className="flex justify-between items-center">
-                <h2 className='text-sm'>45 Responses</h2>
                 <Button className='bg-purple-600 hover:bg-purple-600' size='sm'
                 onClick={()=>ExportData()}
                 disabled={loading}>
-                    
-                    Export</Button>
+                    {loading?<LoaderPinwheel className='animate-spin' />:'Export'}
+                    </Button>
             </div>
         </div>
     )
